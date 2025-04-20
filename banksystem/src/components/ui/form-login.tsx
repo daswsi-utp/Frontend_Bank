203531a6e105238b1da7dc8123296f36bc07111a
JoseUG04 <u20311125@utp.edu.pe>
2025-04-20 18:40:47 -0500

    added to the project the structure for login with zod, nextjs and shadcn, no changes into colors yet

---
+++ banksystem/src/components/ui/form-login.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import { z } from 'zod'
+import { loginSchema } from '@/lib/zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const FormLogin = () => {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof loginSchema>) {
+    console.log(values)
+  }
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Iniciar Sesión</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Correo Electrónico</FormLabel>
+                <FormControl>
+                  <Input placeholder="ingrese su correo electrónico..." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Contraseña</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="Ingrese aquí su contraseña..." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Iniciar Sesión</Button>
+        </form>
+      </Form>
+    </div>
+  )
+}
+
+export default FormLogin
