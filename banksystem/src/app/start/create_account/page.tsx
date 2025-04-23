80a62887ade7dee420de4f40a4b7a0c16d51f871
ArnoldJuarez <arnoljuarez027@gmail.com>
2025-04-23 10:40:03 -0500

    Test2

--- banksystem/src/app/start/create_account/page.tsx
+++ banksystem/src/app/start/create_account/page.tsx
@@ -1,7 +1,7 @@
 export default function CreateAccountPage() {
   return (
     <div>
-      <h1>Crear Cuenta Usuario</h1>
+      <h1>Register Cuenta Usuario</h1>
       {/* Formulario de creación de cuenta */}
     </div>
   )
